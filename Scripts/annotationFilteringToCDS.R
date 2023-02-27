1f160e2dda370876a8259b4c8742435ec443f254
mbarkdull <mb2337@cornell.edu>
2023-02-27 10:10:36 -0500

    Script to filter the reference genome annotations to only CDS features, for input to flo.

---
+++ Scripts/annotationFilteringToCDS.R
@@ -0,0 +1,41 @@
+### I need to filter the CVAR annotation file to just CDS features, to work with flo for annotation remapping. 
+library(tidyverse)
+library(phylotools)
+library(msa)
+library(seqinr)
+
+# Read in the annotation:
+annotation <- read.gff("./CVAR/CVAR_OGS_v1.0_longest_isoform.gff3", 
+                       na.strings = c(".",
+                                      "?"),
+                       GFF3 = TRUE)
+
+# Filter to just CDS features:
+annotationSolelyCDS <- filter(annotation,
+                              type == "CDS")
+
+# Replace NAs in the score column with `.` so that gt doesn't get mad:
+annotationSolelyCDS$score <- as.character(annotationSolelyCDS$score)
+annotationSolelyCDS$score <- "."
+# Likewise, replace NAs in the phase column:
+annotationSolelyCDS$phase <- as.character(annotationSolelyCDS$phase)
+annotationSolelyCDS$phase <- annotationSolelyCDS$phase %>% replace_na("unknown")
+annotationSolelyCDS <- annotationSolelyCDS %>% 
+  mutate(phase = str_replace(phase, "unknown", "."))
+
+# Export the filtered annotation:
+dir.create("./liftoverAnnotation/")
+write.table(annotationSolelyCDS, 
+            file = "./liftoverAnnotation/annotationSolelyCDS.gff3", 
+            append = FALSE, 
+            sep = "\t", 
+            dec = ".",
+            row.names = FALSE, 
+            col.names = FALSE,
+            quote = FALSE)
+
+
+
+
+
+
