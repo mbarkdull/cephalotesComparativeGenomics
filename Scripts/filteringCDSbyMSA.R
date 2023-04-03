e05c070b92472856e8b060e18f91622bf5c74f3c
mbarkdull <mb2337@cornell.edu>
2023-04-03 13:15:36 -0400

    Removing unnecessary command line arguments.

--- Scripts/filteringCDSbyMSA.R
+++ Scripts/filteringCDSbyMSA.R
@@ -1,13 +1,5 @@
 #!/usr/bin/env Rscript
 
-args <- commandArgs(trailingOnly=TRUE)
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).n", call.=FALSE)
-} else if (length(args)==1) {
-  # default output file
-  args[2] = "out.txt"
-}
-
 # Make a directory for outputs:
 dir.create("./filteredCDS")
 
